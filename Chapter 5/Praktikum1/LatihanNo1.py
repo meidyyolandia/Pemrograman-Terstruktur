c4e6125ec4e53090cbda77df3a4c126a3064629a
Meidy Yolandia <89763594+meidyyolandia@users.noreply.github.com>
2021-10-17 21:49:16 +0700

    Delete Chapter 5/Praktikum1 directory

--- Chapter 5/Praktikum1/LatihanNo1.py
+++
@@ -1,8 +0,0 @@
-#Range input nilai adalah 0 - 100
-nilaiBIndo = int(input("Masukkan nilai Bhs Indonesia :"))
-nilaiIPA = int(input("Masukkan nilai IPA           :"))
-nilaiMat = int(input("Masukkan nilai Matematika    :"))
-if (nilaiBIndo > 60) and (nilaiIPA > 60) and (nilaiMat > 70):
-    print("Syarat Kelulusan : LULUS")
-elif (nilaiBIndo < 60) and (nilaiIPA < 60) and (nilaiMat < 70):
-    print("Syarat Kelulusan :TIDAK LULUS")
